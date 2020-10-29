15c1fd707b96c4c1344a6028cc6b68ebc8991566
kubo_hitoshi <kubohti@gmail.com>
2020-10-29 19:33:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Header.h
@@ -0,0 +1,150 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<float.h>
+#include<math.h>
+#include<time.h>
+
+#define ALGO 10 //0:simple  10:NSGA2
+
+#define TYPE 0 //0:contenious  1:discrete
+#define MODE 0//0:benchmark  1:experiment
+
+constexpr int TRIAL = 1;
+
+#define DIM_SEC 15
+#define OBJ_SEC 5
+#define CHROM_SEC 3
+constexpr int POP = 50;
+constexpr int GEN = 500;
+constexpr double CROSSOVER_RATE = 1; // *100[%];
+constexpr double MUTATION_RATE[CHROM_SEC] = { 0.05,0.1 }; // *100[%]
+
+constexpr int discrete[] = { 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120 };
+
+#define EVALUATION 24
+/* EVALUATION FUNCTION
+0:equation_base
+1:simulation_base
+10:SCH
+11:FON
+12:POL
+13:KUR
+24:ZDT4
+50:rosenbrock(2 variables)
+51:sphare(2 variables)
+60:rosenbrock(n variables)
+61:sphare(n variables)
+100:test
+*/
+
+
+#define PSM 0
+/*PARENT SELECTION METHOD
+UNIFORM:0  TORNAMENT:1  ROULETTE:2
+crowding:10
+*/
+
+constexpr int CROSSOVER[CHROM_SEC] = { 11,102 };
+/* CROSSOVER METHOD
+OnePX:0  TwoPX:1  UniX:2
+BLX_a:10  SBX:11
+STL_2PX:100  STL_SX:101  STL_BLX_a:102
+*/
+constexpr int MUTATION[CHROM_SEC] = { 2,101 };
+//なし:0　UNIM:1 PBM:2 100:STL_descrete 101:STL_continious;
+
+#define TORNAMENT_SIZE 2
+
+typedef struct INDIVIDUAL {
+	double X[DIM_SEC][CHROM_SEC];
+	double f[OBJ_SEC];
+	double fitness[OBJ_SEC];
+	double w[OBJ_SEC];
+	double evaluation;
+	int eval_tag; // 0:未評価 1:評価済み
+
+	int dominated;
+	double crowding_distance;
+	int dom_ind[POP + POP];
+	int domination;
+	int rank;
+}individual;
+
+typedef struct FRONT {
+	individual F[POP + POP][POP + POP]; //[ランク][個体配列]
+	int num_arr[POP + POP]; //ランク内個体数
+	int rank_arr; //総ランク数
+}front;
+
+//grobal variance
+extern front F;
+extern individual P[POP];
+extern individual nextP[POP];
+extern individual Q[POP];
+extern individual R[POP + POP];
+extern individual best_P[GEN];
+extern individual elite[2];
+extern double gen_ave[GEN];
+extern double upper_bound[DIM_SEC][CHROM_SEC];
+extern double lower_bound[DIM_SEC][CHROM_SEC];
+extern int DIM[CHROM_SEC];
+extern int OBJ;
+extern int CHROM;
+extern int max_min[OBJ_SEC];
+extern int MIN_OR_MAX[OBJ_SEC];
+
+//algorithms
+void basic_GA();
+void NSGA2();
+
+//crossover
+void crossover(individual* B);
+
+//evaluation
+void evaluation(individual* A, int arr);
+
+//main
+void initialize3(individual* A, int arr);
+void initialize2();
+void initialize(individual* A, int arr);
+void record(individual* ind, int arr, int cnt);
+void init_ind(individual* ind, int arr);
+
+
+//mutation
+void mutation(individual* B);
+
+//process
+void random_seed();
+void non_restored_extract(int max, int* x, int arr);
+void tornament_select(individual* B, int max, int* x, int arr);
+void roulette_select(individual* B, int limit, int* num, int arr);
+void crowding_tornament_select(individual* B, int max, int* x, int arr);
+int uniform_random(int a);
+int find_header(FILE* fp, char* keyword);
+int find_header2(FILE* fp, char* keyword);
+void sort(individual* ind, int arr);
+void d_sort(double* x, int arr);
+void clear_array(double* x, int arr);
+void clear_POP(individual* A, int arr);
+void ind_cpy(individual* A, individual* B, int arr);
+void individual_info(individual ind);
+
+
+//setting
+void setting();
+void experiment_setting();
+void benchmark_setting();
+
+//simulation
+void make_netlist(individual* A, int arr);
+void make_netlist2(individual* A, int arr);
+void sim_STL(individual* A, int arr);
+double get_result(int i);
+double getwidth(double X[][CHROM_SEC]);
+
+
+
---
+++ main.cpp
@@ -0,0 +1,377 @@
+#include"Header.h"
+void read_setting();
+void random_weight();
+void fix_weight();
+void output_result(double ex_time);
+
+
+int main(void) {
+	int it = 0;
+	clock_t start, end;
+	double ex_time;
+
+	do {
+		clear_POP(P, POP);
+		clear_POP(Q, POP);
+		clear_POP(R, POP + POP);
+		elite[0].f[0] = DBL_MAX;
+		elite[1].f[0] = DBL_MAX;
+		start = clock();
+		setting(); //問題設定の読み込み
+		random_seed(); //乱数初期値
+		if (ALGO == 0) {
+			basic_GA();
+		}
+		else if (ALGO == 10) {
+			NSGA2();
+		}
+		else {
+			printf("check ALGORITHM\n");
+			exit(1);
+		}
+		end = clock();
+		ex_time = (double)(end - start) / CLOCKS_PER_SEC;
+		printf("%.2f秒かかりました\n", ex_time);
+		output_result(ex_time);
+		it++;
+	} while (it < TRIAL);
+	//*******************
+	for (int i = 0; i < POP; i++) {
+		printf("ind[%d]\n", i);
+		individual_info(P[i]);
+	}
+	//*******************
+
+	return 0;
+}
+
+void read_setting() {
+	FILE* fp;
+	errno_t error;
+	//int check;
+
+	//test
+	//char objective[64] = "#objective";
+	/*char min_or_max[64] = "#min_or_max";*/
+
+	error = fopen_s(&fp, "setting.txt", "r");
+	if (error != 0) {
+		printf("設定ファイルを開けませんでした");
+		exit(1);
+	}
+
+	//find_header(fp, objective);
+	//fscanf_s(fp, "%d", &OBJ);
+
+	//find_header(fp, min_or_max);
+	//for (int i = 0; i < OBJ; i++) {
+	//	fscanf_s(fp, "%d", &MIN_OR_MAX[i]);
+	//}
+
+	fclose(fp);
+}
+
+
+void FWGA() {
+	//crossover();
+	//mutation();
+	//evaluation();
+}
+//
+//
+//void SOGA() {
+//	
+//	innerGA();
+//	//outerGA
+//	crossover();
+//	mutation();
+//	evaluation();
+//
+//}
+//
+//void innerGA() {
+//	double w[pop_inner][num_objective];
+//	for (i = 0; i < 20; i++) {
+//		w_evaluate(w);
+//		crossover(w);
+//		mutation(w);
+//	}
+//	choose_best_w();
+//}
+//
+//void crossover() {
+//	roulette();
+//}
+
+void initialize(individual* A, int arr) {
+	int i, j, k;
+	for (i = 0; i < arr; i++) {
+		for (j = 0; j < CHROM; j++) {
+			for (k = 0; k < DIM[j]; k++) {
+				A[i].X[k][j] = lower_bound[k][j] + (double)rand() / RAND_MAX * (upper_bound[k][j] - lower_bound[k][j]);
+			}
+		}
+	}
+
+}
+
+void initialize2() {
+	int i, j;
+	int temp;
+	double sum;
+
+	for (i = 0; i < POP; i++) {
+		for (j = 0; j < 9; j++) {
+			temp = rand() % 3;
+			if (temp == 0) {
+				P[i].X[j][0] = 5;
+			}
+			else if (temp == 1) {
+				P[i].X[j][0] = 25;
+			}
+			else if (temp == 2) {
+				P[i].X[j][0] = 76;
+			}
+			else {
+				exit(1);
+			}
+		}
+		for (j; j < 11; j++) {
+			P[i].X[j][0] = (double)(rand() % 99 + 1);
+		}
+		for (j = 0; j < DIM[1]; j++) {
+			P[i].X[j][1] = lower_bound[j][1] + (double)rand() / RAND_MAX * (upper_bound[j][1] - lower_bound[j][1]);
+		}
+		sum = P[i].X[0][1] + P[i].X[1][1] + P[i].X[2][1];
+		P[i].X[0][1] = P[i].X[0][1] / sum * 280;
+		P[i].X[1][1] = P[i].X[1][1] / sum * 280;
+		P[i].X[2][1] = P[i].X[2][1] / sum * 280;
+		sum = P[i].X[3][1] + P[i].X[4][1] + P[i].X[5][1];
+		P[i].X[3][1] = P[i].X[3][1] / sum * 224;
+		P[i].X[4][1] = P[i].X[4][1] / sum * 224;
+		P[i].X[5][1] = P[i].X[5][1] / sum * 224;
+		sum = P[i].X[6][1] + P[i].X[7][1] + P[i].X[8][1];
+		P[i].X[6][1] = P[i].X[6][1] / sum * 56;
+		P[i].X[7][1] = P[i].X[7][1] / sum * 56;
+		P[i].X[8][1] = P[i].X[8][1] / sum * 56;
+	}
+}
+
+void initialize3(individual* A, int arr) {
+	int i, j;
+	int temp;
+	double sum;
+
+	int arrnum = sizeof discrete / sizeof(int);
+	for (i = 0; i < arr; i++) {
+		if (TYPE == 0) {
+			for (j = 0; j < DIM[0]; j++) {
+				A[i].X[j][0] = lower_bound[j][0] + (double)rand() / RAND_MAX * (upper_bound[j][0] - lower_bound[j][0]);
+			}
+		}
+		else if (TYPE == 1) {
+			for (j = 0; j < 9; j++) {
+				temp = rand() % arrnum;
+				A[i].X[j][0] = discrete[temp];
+			}
+			for (j; j < 11; j++) {
+				A[i].X[j][0] = (double)(rand() % 99 + 1);
+			}
+		}
+		else {
+			printf("TYPE value is invalid.\n");
+			exit(1);
+		}
+
+		for (j = 0; j < DIM[1]; j++) {
+			A[i].X[j][1] = lower_bound[j][1] + (double)rand() / RAND_MAX * (upper_bound[j][1] - lower_bound[j][1]);
+		}
+		sum = A[i].X[0][1] + A[i].X[1][1] + A[i].X[2][1];
+		A[i].X[0][1] = A[i].X[0][1] / sum * 280;
+		A[i].X[1][1] = A[i].X[1][1] / sum * 280;
+		A[i].X[2][1] = A[i].X[2][1] / sum * 280;
+		sum = A[i].X[3][1] + A[i].X[4][1] + A[i].X[5][1];
+		A[i].X[3][1] = A[i].X[3][1] / sum * 224;
+		A[i].X[4][1] = A[i].X[4][1] / sum * 224;
+		A[i].X[5][1] = A[i].X[5][1] / sum * 224;
+		sum = A[i].X[6][1] + A[i].X[7][1] + A[i].X[8][1];
+		A[i].X[6][1] = A[i].X[6][1] / sum * 56;
+		A[i].X[7][1] = A[i].X[7][1] / sum * 56;
+		A[i].X[8][1] = A[i].X[8][1] / sum * 56;
+
+	}
+
+}
+
+
+void random_weight() {
+	double total;
+	int i, j;
+	for (i = 0; i < POP; i++) {
+		total = 0;
+		for (j = 0; j < OBJ; j++) {
+			P[i].w[j] = (double)rand();
+			total += P[i].w[j];
+		}
+		for (j = 0; j < OBJ; j++) {
+			P[i].w[j] = P[i].w[j] / total;
+		}
+	}
+}
+
+void fix_weight() {
+	int i, j;
+	for (i = 0; i < POP; i++) {
+		P[i].w[0] = 0.1;
+		P[i].w[1] = 0.9;
+	}
+}
+
+
+
+void record(individual* ind, int arr, int cnt) {
+	int i, j;
+	double keep = 0;
+	double ave = 0;
+	for (i = 0; i < arr; i++) {
+		if (keep < ind[i].evaluation) {
+			keep = ind[i].evaluation;
+			best_P[cnt] = ind[i];
+		}
+	}
+	for (i = 0; i < arr; i++) {
+		ave += ind[i].f[0];
+	}
+	ave = ave / (double)arr;
+	gen_ave[cnt] = ave;
+	sort(ind, arr);
+	if (elite[0].f[0] > ind[0].f[0]) {
+		elite[1] = elite[0];
+		elite[0] = ind[0];
+	}
+	if (elite[1].f[0] > ind[1].f[0]) {
+		elite[1] = ind[1];
+	}
+
+}
+
+void output_result(double ex_time) {
+	time_t timer;
+	struct tm local;
+	int i, j, k;
+	char filename[100];
+	char filename2[100];
+	FILE* fp, * fp2;
+	errno_t error, error2;
+	errno_t errort;
+
+	timer = time(NULL);
+	errort = localtime_s(&local, &timer);
+
+	if (errort != 0) {
+		sprintf_s(filename, "result.txt");
+	}
+	else {
+		sprintf_s(filename, "C:/Users/kuboh/source/repos/SOGA/result/result%2d%2d%d%d.txt", local.tm_mon + 1, local.tm_mday, local.tm_hour, local.tm_min);
+	}
+
+	error = fopen_s(&fp, filename, "w");
+	if (error != 0) {
+		printf("結果ファイル.txtを開けませんでした。");
+		exit(1);
+	}
+	else {
+		if (errort != 0) {
+			fprintf(fp, "TIME ERROR\n");
+		}
+		else {
+			fprintf(fp, "%4d/", local.tm_year + 1900);
+			fprintf(fp, "%2d/", local.tm_mon + 1);
+			fprintf(fp, "%2d ", local.tm_mday);
+			fprintf(fp, "%2d:", local.tm_hour);
+			fprintf(fp, "%2d:", local.tm_min);
+			fprintf(fp, "%2d\n", local.tm_sec);
+		}
+		fprintf(fp, "実行時間 : %.2fs\n", ex_time);
+		fprintf(fp, "*******************************\n");
+		fprintf(fp, "MODE : %d\n", MODE);
+		fprintf(fp, "EVALUATION : %d\n", EVALUATION);
+		fprintf(fp, "POPULATION : %d\n", POP);
+		fprintf(fp, "GENERATION : %d\n", GEN);
+		for (i = 0; i < CHROM; i++) {
+			fprintf(fp, "CROSSOVER_METHOD[%d] : %d\n", i, CROSSOVER[i]);
+		}
+		fprintf(fp, "CROSSOVER_RATE : %.3f\n", CROSSOVER_RATE);
+		for (i = 0; i < CHROM; i++) {
+			fprintf(fp, "MUTATION_METHOD[%d] : %d\n", i, MUTATION[i]);
+		}
+		for (i = 0; i < CHROM; i++) {
+			fprintf(fp, "MUTATION_RATE[%d] : %.3f\n", i, MUTATION_RATE[i]);
+		}
+		fprintf(fp, "*******************************\n");
+
+
+
+		for (j = 0; j < GEN; j++) {
+			fprintf(fp, "gen[%d] ", j);
+			for (k = 0; k < CHROM; k++) {
+				for (i = 0; i < DIM[k]; i++) {
+					fprintf(fp, "%f ", best_P[j].X[i][k]);
+				}
+			}
+			for (i = 0; i < OBJ; i++) {
+				fprintf(fp, "f[%d]=%f ", i, best_P[j].f[i]);
+			}
+			fprintf(fp, "eval=%f ave=%f\n", best_P[j].evaluation, gen_ave[j]);
+		}
+	}
+	fclose(fp);
+
+	//ここからcsvファイル出力
+	sprintf_s(filename2, "C:/Users/kuboh/source/repos/SOGA/result/final%2d%2d%d%d.csv", local.tm_mon + 1, local.tm_mday, local.tm_hour, local.tm_min);
+	error2 = fopen_s(&fp2, filename2, "w");
+	if (error2 != 0) {
+		printf("結果ファイル.csvを開けませんでした。");
+		exit(1);
+	}
+	else {
+		//見出し
+		fprintf(fp2, "result,");
+		for (i = 0; i < CHROM; i++) {
+			for (j = 0; j < DIM[i]; j++) {
+				fprintf(fp2, "X[%d][%d],", j, i);
+			}
+		}
+		for (j = 0; j < OBJ; j++) {
+			fprintf(fp2, "f[%d],", j);
+		}
+		fprintf(fp2, "eval\n");
+		//データ
+		for (j = 0; j < POP; j++) {
+			fprintf(fp2, "P[%d],", j);
+			for (k = 0; k < CHROM; k++) {
+				for (i = 0; i < DIM[k]; i++) {
+					fprintf(fp2, "%f,", P[j].X[i][k]);
+				}
+			}
+			for (i = 0; i < OBJ; i++) {
+				fprintf(fp2, "%f,", P[j].f[i]);
+			}
+			fprintf(fp2, "%f\n", P[j].evaluation);
+		}
+	}
+	fclose(fp2);
+
+
+}
+
+void init_ind(individual* ind, int arr) {
+	if (MODE == 0) {
+		initialize(ind, arr);
+	}
+	if (MODE == 1) {
+		initialize3(ind, arr);
+	}
+}
+
+
